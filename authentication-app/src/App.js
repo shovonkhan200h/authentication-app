3fb6b4bc156d4e41823bc9bcfdcb0dd2fed7d890
shovonkhan200h <shovonkhan00@gmail.com>
2023-10-12 04:18:47 +0100

    update firebase authentication google singed in and singed Out

--- authentication-app/src/App.js
+++ authentication-app/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import {  Button, Container, Grid, Typography } from '@mui/material'
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from "./Component/Configaration/Configaration";
+const app = initializeApp(firebaseConfig)
+
+
+
+
+
 
 function App() {
+  const [user, setUser] = React.useState({
+    isSingedIN: false,
+    name: "",
+    email: ''
+  })
+  const provider = new GoogleAuthProvider();
+  const handleBtn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+
+        const { displayName, email } = result.user;
+        const singedInUser = {
+          isSingedIN: true,
+          name: displayName,
+          email: email
+        }
+        setUser(singedInUser)
+
+      }).catch((error) => {
+
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+
+  const singOut = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      const user = {
+        isSingedIN:false,
+        name:'',
+        email:''
+      }
+      setUser(user)
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='lg'>
+      <Grid container spacing={2} sx={{background:'black',color:'#fff'}}>
+        <Grid item xs={10}>
+        {
+        user.isSingedIN ? <Button onClick={() => {singOut() }}>SingOut</Button>
+          : <Button onClick={() => { handleBtn() }}>SingIn</Button>
+          }
+        </Grid>
+        <Grid item xs={2}>
+        {
+        user.isSingedIN && <Typography>{user.name}</Typography> 
+        }
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ authentication-app/src/Component/Configaration/Configaration.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBrJnSiPskqUbghw4Ch_RJJGmlcUMZP0TQ",
+    authDomain: "online-shop-48542.firebaseapp.com",
+    projectId: "online-shop-48542",
+    storageBucket: "online-shop-48542.appspot.com",
+    messagingSenderId: "545529172917",
+    appId: "1:545529172917:web:33c379fb237f17b4fffffc"
+  };
+
+export default firebaseConfig;
